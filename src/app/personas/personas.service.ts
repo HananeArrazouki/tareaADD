5fa122dae922f13fd8182473263778bd6f1258d7
HananeArrazouki <hanane.arrazouki1999@gmail.com>
2022-10-16 18:51:56 +0200

    Fase2

--- src/app/personas/personas.service.ts
+++ src/app/personas/personas.service.ts
@@ -5,20 +5,19 @@ export interface Persona{
   apellido : string;
   edad : number;
   id : number;
-  apodo? : string;
+  apodo : string;
   img? : string;
 }
-//https://drive.google.com/drive/folders/1J3iLf3BvtlIFGyvSL_o_nZtj01x-2WNR?usp=sharing
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonasService {
 
-  
   public persona: Persona[] = [
-    {nombre: 'Hanane', apellido: 'Arrazouki', edad: 23, id: 1, apodo: 'Baka'},
-    {nombre: 'Sergio', apellido: 'García', edad: 23, id: 2, apodo: 'SeryiDev'},
+    {nombre: 'Hanane', apellido: 'Arrazouki', edad: 23, id: 1, apodo: 'NO tiene', img: "https://play-lh.googleusercontent.com/M8Ynjs9CjnwtG04ilnUqzdfCvLwvZsbaCMRlL8aFx4gKWEKKef02kV1GSwm07gdmW7Kt"},
+    {nombre: 'Sergio', apellido: 'García', edad: 23, id: 2, apodo: 'SeryiDev', img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpSxXnVvkhIxyUTgn3phii_X9n6F2dsise2g&usqp=CAU"},
     {nombre: 'Verónica', apellido: 'González', edad:19, id: 3, apodo: 'Vero'},
     {nombre: 'Natalia', apellido: 'Castillo', edad:45, id: 4, apodo: 'NO tiene'},
     {nombre: 'Alejandro', apellido: 'Santos', edad:20, id: 5, apodo: 'Alex'}    
