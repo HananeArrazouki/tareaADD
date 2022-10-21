52a4c3536c878cb03e6725100d596ca96af02c26
HananeArrazouki <hanane.arrazouki1999@gmail.com>
2022-10-21 10:18:23 +0200

    Fase 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
