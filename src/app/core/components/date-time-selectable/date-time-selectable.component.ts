48e24d022f1c5eaa43c2e37126402e60746f4ebb
HananeArrazouki <hanane.arrazouki1999@gmail.com>
2022-11-08 09:04:05 +0100

    Fase 6

--- src/app/core/components/date-time-selectable/date-time-selectable.component.ts
+++ src/app/core/components/date-time-selectable/date-time-selectable.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR } from '@angular/forms';
 import { IonAccordionGroup, IonDatetime } from '@ionic/angular';
 import { Persona } from '../../interfaces/persona';
+
+const DATETIME_PROFILE_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => DateTimeSelectableComponent),
+  multi: true
+}
 
 @Component({
   selector: 'app-date-time-selectable',
   templateUrl: './date-time-selectable.component.html',
   styleUrls: ['./date-time-selectable.component.scss'],
+  providers: [DATETIME_PROFILE_VALUE_ACCESSOR]
 })
 export class DateTimeSelectableComponent implements OnInit {
   
